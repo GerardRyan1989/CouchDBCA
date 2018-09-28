94d929cd66dde9c3d433a70e9564f8056e8e3551
GerardRyan1989 <gerardryan1989@gmail.com>
2018-09-28 01:34:27 +0100

    updated person class to the car class also added a previois owner and service history class

--- DataLayer/Car.cs
+++ DataLayer/Car.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,10 +7,44 @@ using System.Threading.Tasks;
 
 namespace CouchDBCA
 {
-    public class Car
+    public class Person
     {
-        public string make { get; set; }
-        public double engineSize { get; set; }
-        public string fuelType { get; set; }
+        [JsonProperty("_id")]
+        public string _id { get; set; }
+
+        [JsonProperty("_rev")]
+        public string _rev { get; set;}
+
+        [JsonProperty("name")]
+        public string name { get; set; }
+
+        [JsonProperty("age")]
+        public string age { get; set; }
+
+
+        [JsonProperty("characterisitics")]
+        public List<string> characteristics { get; set; }
+
+        [JsonProperty("personCar")]
+        public Car personcar { get; set; }
+
+        
+        //[JsonProperty("")]
+        //[JsonProperty("")]
+        //[JsonProperty("")]
+        //
+        //[JsonProperty("")]
+        //[JsonProperty("")]
+        //[JsonProperty("")]
+        //[JsonProperty("")]
+        //[JsonProperty("")]
+
+
+        public void checkIfGithubLinked()
+        {
+            Console.Write("github");
+        }
+
+
     }
 }
